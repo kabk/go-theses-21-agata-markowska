dbdc6ebcc2529a6745e3760f3024224aeb71089d
AMarkowska <55256983+AMarkowska@users.noreply.github.com>
2021-01-07 00:03:49 +0100

    v3
    new option with dot and all

---
+++ mouse_dot_2.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+               var bsDiv = document.getElementById("circle");
+               var x, y;
+   // On mousemove use event.clientX and event.clientY to set the location of the div to the location of the cursor:
+               window.addEventListener('mousemove', function(event){
+                   x = event.clientX;
+                   y = event.clientY;
+                   if ( typeof x !== 'undefined' ){
+                       bsDiv.style.left = x + "px";
+                       bsDiv.style.top = y + "px";
+                   }
+               }, false);
+           }
---
+++ no_cursor.js
@@ -0,0 +1,1 @@
+document.getElementById('nocursor').style.cursor = 'none';
